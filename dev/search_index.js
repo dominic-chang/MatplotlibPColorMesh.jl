7ddaae3220349c08c88e2662ab924dc83a555e65
Documenter.jl <documenter@juliadocs.github.io>
2023-08-24 10:28:52 +0000

    build based on e5e6ff8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PColorMesh","category":"page"},{"location":"#PColorMesh","page":"Home","title":"PColorMesh","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PColorMesh.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PColorMesh]","category":"page"}]
+}
